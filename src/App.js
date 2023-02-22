33b9970d750d6358aaf1f4136e6cd81f8a9cb38b
Dany serfaty <davidserfaty2315@gmail.com>
2023-02-22 18:26:35 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [listes,setListes] = useState([])
+
+
+
+  
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      TODO list
+      </header>
+
+      <input type="text"  />
+     
+
+    </div>
+  );
+}
+
+export default App;
